c1c6fa44df26471d91fdfdaaef4cb89d2cb5dfc2
Henrique Ramos <henrique.mrcr@gmail.com>
2023-10-13 14:18:26 +0100

    some errors

--- assets/auth.js
+++ assets/auth.js
@@ -28,40 +28,4 @@ async function login(e) {
     .catch((x) => alert(x.message));
 }
 
-async function register(e) {
-  e.preventDefault();
-
-  const form = e.target;
-
-  const body = {
-    email: form.name.email,
-    name: form.name.value,
-    gender: form.gender.value,
-    password: form.password.value,
-  };
-
-  try {
-    const response = await fetch(`${API_URL}/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(body),
-    });
-
-    if (response.ok) {
-      alert("Registration successful. You can now log in.");
-    } else {
-      const errorData = await response.json();
-      alert("Registration error: " + errorData.message);
-    }
-  } catch (error) {
-    alert("An error occurred: " + error.message);
-  }
-
-}
-
 document.getElementById("login").addEventListener("submit", login);
-
-document.getElementById("singup").addEventListener("submit", signup);
---
+++ assets/signup.js
@@ -0,0 +1,36 @@
+async function register(e) {
+  e.preventDefault();
+
+  const form = e.target;
+
+  const body = {
+    email: form.email.value,
+    name: form.name.value,
+    gender: form.gender.value,
+    password: form.password.value,
+  };
+
+  console.log(1111111, body);
+
+  try {
+    const response = await fetch(`${API_URL}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      alert("Registration successful. You can now log in.");
+    } else {
+      const errorData = await response.json();
+      alert("Registration error: " + errorData.message);
+    }
+  } catch (error) {
+    alert("An error occurred: " + error.message);
+  }
+}
+
+document.getElementById("signup").addEventListener("submit", register);
